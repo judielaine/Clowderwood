a7ce05a8e7de0a21073ef43d98dae82c42ecabc7
Judielaine <git@grey-cat.com>
2024-06-22 10:07:08 -0400

    refactor: tidying up to separate the analysis from the data & doc

---
+++ _Analysis/clowderwoodpy/__init__.py
@@ -0,0 +1,6 @@
+# Clowderwood Python scripts
+#
+# Created: 2024/05/24 08:16:45
+# Last modified: 2024/06/22 10:03:13
+# 
+# Helpers for the Jupyter notebooks and binders
--- clowderwoodpy/__init__.py
+++
@@ -1,7 +0,0 @@
-# Clowderwood Python scripts
-#
-# Created: 2024/05/24 08:16:45
-# Last modified: 2024/06/22 10:01:06
-# 
-# Helpers for the Jupyter notebooks and binders
-
